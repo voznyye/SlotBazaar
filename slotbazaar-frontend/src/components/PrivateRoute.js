2590ec907bfd1c098a535b4d6b96cdd0883e8097
terozalent <8l3ndz10r@protonmail.com>
2025-06-07 18:48:37 +0200

    Revert "Added frontend"
    This reverts commit f09ceaec5985e37bd6e7418fef09e3afd7d5d7d8.

--- slotbazaar-frontend/src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
-
-export default function PrivateRoute({ children }) {
-  const { user } = useContext(AuthContext);
-  return user ? children : <Navigate to="/login" />;
-}
--- slotbazaar-frontend/src/markdown/terms.md
+++
@@ -1,7 +0,0 @@
-# Terms and Conditions
-
-Welcome to SlotBazaar! Please read these terms carefully...
-
-## 1. Eligibility
-
-You must be 18 years or older...
